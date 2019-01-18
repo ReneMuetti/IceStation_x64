8aedaebbff72194a07eff2efaf614103674d38ac
ReneMuetti <ReneMuetti@googlemail.com>
2019-01-18 08:58:04 +0100

    * add Comments and Documentation

--- web/global.php
+++ web/global.php
@@ -1,14 +1,34 @@
 <?php
-include_once realpath('./config.php');
+/**
+ * Loading Pre-Configuration
+ */
+include_once realpath('./include/configs/config_data.php');
 
+/**
+ * defined custom Constants
+ */
 define ('INC_PATH'  , $config['base_path'] . '/include' );
+define ('CONF_PATH' , $config['base_path'] . '/include/configs' );
 define ('CLASS_PATH', $config['base_path'] . '/include/classes' );
 define ('FUNC_PATH' , $config['base_path'] . '/include/functions' );
 
+/**
+ * complete Configuration
+ */
 $config['baseurl']    = $config['protocol'] . '://' . $config['host'] . $config['script'];
 $config['inc_path']   = INC_PATH;
+$config['conf_path']  = CONF_PATH;
 $config['class_path'] = CLASS_PATH;
 $config['func_path']  = FUNC_PATH;
 
+/**
+ * Class-Loader
+ */
 include_once INC_PATH . '/autoloader.php';
+
+/**
+ * Init
+ */
+$site = new Registry();
+$site -> fetch_config();
 ?>
--- web/index.php
+++ web/index.php
@@ -2,9 +2,16 @@
 error_reporting(E_ALL & ~ E_NOTICE);
 ini_set ('display_errors', 'On');
 
+// #################### DEFINE IMPORTANT CONSTANTS #######################
+define('THIS_SCRIPT', 'index');
+
+// ######################### REQUIRE BACK-END ############################
 require_once './global.php';
 
+// #######################################################################
+// ######################## START MAIN SCRIPT ############################
+// #######################################################################
 echo '<pre>';
-var_dump($config);
+var_dump($site);
 echo '</pre>';
 ?>
